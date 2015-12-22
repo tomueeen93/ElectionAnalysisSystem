1ab9a049b0145d25e753096be5c5a063df3929d8
yu xinlu <yu xinlu>
2015-12-22 17:17:12 +0900

    Modified ldirectory structures

--- src/mainja1.py
+++ src/mainja1.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import sys,os
+sys.path.append('./lib')
+
 from requests_oauthlib import OAuth1Session
 import csv
 import json
---
+++ src/sample.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import sys,os
+sys.path.append('../lib')
+
+
+from requests_oauthlib import OAuth1Session
+import csv
+import json
+import os
+import MeCab
+import time
+
